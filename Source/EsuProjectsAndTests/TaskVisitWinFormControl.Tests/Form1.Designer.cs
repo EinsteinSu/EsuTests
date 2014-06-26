7ce44ffd3fa80222d54a994b8d5a87810ad0ca91
EinsteinSu <einstein_supeng@hotmail.com>
2014-06-26 16:51:41 +0800

    async test project

---
+++ Source/EsuProjectsAndTests/TaskVisitWinFormControl.Tests/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TaskVisitWinFormControl.Tests
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
+      ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // comboBoxEdit1
+      // 
+      this.comboBoxEdit1.Location = new System.Drawing.Point(12, 12);
+      this.comboBoxEdit1.Name = "comboBoxEdit1";
+      this.comboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+      this.comboBoxEdit1.Size = new System.Drawing.Size(260, 20);
+      this.comboBoxEdit1.TabIndex = 0;
+      this.comboBoxEdit1.Popup += new System.EventHandler(this.comboBoxEdit1_Popup);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.Controls.Add(this.comboBoxEdit1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
+  }
+}
+
---
+++ Source/EsuProjectsAndTests/TaskVisitWinFormControl.Tests/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskVisitWinFormControl.Tests
+{
+  public partial class Form1 : Form
+  {
+    private TaskScheduler scheduler;
+    public Form1()
+    {
+      InitializeComponent();
+      scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      var parent = Task.Factory.StartNew(() =>
+      {
+        var ipEndPoint = new IPEndPoint(IPAddress.Broadcast, 7985);
+        Byte[] bytes = Encoding.UTF8.GetBytes("quest:discover");
+        using (var udpClient = new UdpClient())
+        {
+          udpClient.Send(bytes, bytes.Length, ipEndPoint);
+          var datetime = DateTime.Now;
+          var timeSpan = new TimeSpan(0, 0, 10);
+          while ((DateTime.Now - datetime) <= timeSpan)
+          {
+            var receiveBytes = udpClient.Receive(ref ipEndPoint);
+            var task = Task<string>.Factory.StartNew(() =>
+            {
+              string returnData = Encoding.UTF8.GetString(receiveBytes);
+              if (!string.IsNullOrEmpty(returnData))
+              {
+                string[] splits = returnData.Split(new[] { ':' });
+                if (splits.Length == 5)
+                  return splits[2];
+              }
+              return string.Empty;
+            });
+            task.ContinueWith(t =>
+            {
+              if (!string.IsNullOrEmpty(t.Result))
+              {
+                comboBoxEdit1.Properties.Items.Add(t.Result);
+              }
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, scheduler);
+          }
+        }
+      });
+      parent.ContinueWith((task) =>
+      {
+        MessageBox.Show(@"error has happened!");
+      }, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, scheduler);
+    }
+
+    private void comboBoxEdit1_Popup(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
